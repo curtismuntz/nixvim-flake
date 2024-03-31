58fbad0f6bf10e665a5d4e11dc53c417e31f0140
murt <me@murt.is>
2024-03-30 19:56:36 -0700

    add telescope, lsp, treesitter

--- config/default.nix
+++ config/default.nix
@@ -2,5 +2,10 @@
   # Import all your configuration modules here
   imports = [
     ./bufferline.nix
+    ./treesitter.nix
+    ./telescope.nix
+    ./lsp.nix
   ];
+  colorschemes.gruvbox.enable = true;
+  globals.mapleader = " ";
 }
---
+++ config/lsp.nix
@@ -0,0 +1,19 @@
+plugins.lsp = {
+      enable = true;
+
+      servers = {
+        tsserver.enable = true;
+	ccls.enable = true;
+
+        lua-ls = {
+          enable = true;
+          settings.telemetry.enable = false;
+        };
+
+        rust-analyzer = {
+          enable = true;
+          installCargo = true;
+        };
+      };
+};
+
